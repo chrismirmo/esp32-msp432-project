362cbb0bd95266efc5b9074b890955a113715c08
chrismirmo <129929594+chrismirmo@users.noreply.github.com>
2026-09-18 16:00:34 -0500

    Create secrets-example.h
    A template header file for defining the cryptographic keys that the microcontrollers will use to secure their communications.

---
+++ secrets-example.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_H_
+#define SECRETS_H_
+
+#include <stdint.h>
+
+static const uint8_t KEY_AUTH[16] = { 0x00 /* Replace with 16-byte key */ };
+static const uint8_t KEY_DATA[16] = { 0x00 /* Replace with 16-byte key */ };
+
+#endif
